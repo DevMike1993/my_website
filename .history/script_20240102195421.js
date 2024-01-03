46acc42c96695df6c1915fce63f8c03d7c3097f9
collecting_experience <m.bulinski93@gmail.com>
2024-01-03 17:27:19 +0100

    Moja Strona

---
+++ .history/script_20240102195421.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'flex' : 'none';
+}
+
+// Oznacz bieżącą stronę jako aktywną w menu
+document.addEventListener("DOMContentLoaded", function() {
+    const path = window.location.pathname;
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    menuItems.forEach(item => {
+        if (item.getAttribute('href') === path) {
+            item.classList.add('active');
+        }
+    });
+});
---
+++ .history/script_20240102211653.js
@@ -0,0 +1,11 @@
+// const menuIcon = document.querySelector(".menu-icon");
+//     const menu = document.querySelector(".menu");
+
+//     menuIcon.addEventListener("click", function () {
+//         menu.classList.toggle("show");
+//     });
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("show");
+}
